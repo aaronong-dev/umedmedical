977b170ef2dbe1b821cb9fd758a43cc8b4c10177
aaron-vong <aaronong74@gmail.com>
2026-02-13 19:04:48 -0600

    Added footer divider

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,11 @@ export default function Footer() {
 
   return (
     <footer className="footer">
+      <div className="footer__wave" aria-hidden="true">
+        <svg viewBox="0 0 1440 48" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 24c120 0 240-24 360-24s240 24 360 24 240-24 360-24 240 24 360 24v24H0V24z" fill="currentColor" />
+        </svg>
+      </div>
       <div className="footer__content">
         <a href="/" className="footer__brand" aria-label="UMed Medical home">
           <img src="/logo.png" alt="" className="footer__logo" />
